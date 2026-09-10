9e59ef23cb6ddf1788d12af44a3d61be6a0ae988
SayedMOO <anaelsaid@gmail.com>
2026-09-11 00:34:00 +0300

    chore: setup project and dependencies

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express =require("express");
+const app =express();
+const cors =require("cors");
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "API is running",
+  });
+});
+
+module.exports = app;
