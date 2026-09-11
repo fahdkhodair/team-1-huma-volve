138a041619b66d181e8dba22e91209fdd4aec279
Adam Ibrahim <adamdomadoma@gmail.com>
2026-09-11 18:24:53 +0300

    Add refresh token route to authentication endpoints

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,17 +1,25 @@
 import { Router } from "express";
+
 import {
   Login,
   Register,
   Logout,
-  GetCurrentUser
+  GetCurrentUser,
+  RefreshToken,
 } from "../controllers/authController.js";
+
 import authMiddleware from "../middleware/authMiddleware.js";
 
- const authRouter = Router();
-
+const authRouter = Router();
 
 authRouter.post("/login", Login);
+
 authRouter.post("/register", Register);
+
+authRouter.post("/refresh", RefreshToken);
+
 authRouter.post("/logout", authMiddleware, Logout);
+
 authRouter.get("/me", authMiddleware, GetCurrentUser);
+
 export default authRouter;
