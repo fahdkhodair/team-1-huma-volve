823846ddd6258580a43317bdc635ac885c250661
Fahd Khodair <khodairfahd@gmail.com>
2026-09-12 11:05:55 -0700

    add

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log("MONGO_URI:", process.env.MONGO_URI);
     } catch (error) {
         console.error('Database connection error:', error);
         process.exit(1);
