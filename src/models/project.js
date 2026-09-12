338af3a587039252b106bf633c5a5eaf8ae312ac
Fahd Khodair <khodairfahd@gmail.com>
2026-09-12 06:15:50 -0700

    update

---
+++ src/models/project.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+const projectSchema = new mongoose.Schema({
+       Name:{
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+       maxlength: 100,
+    },
+    description:{
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 10,
+        maxlength: 2000,
+    },
+      owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    Memebers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    stauts: {
+        type: String,
+        enum: ["active", "inactive"],
+        default: "active",
+    },
+},{timestamps: true})
+module.exports = mongoose.model("Project", projectSchema)
