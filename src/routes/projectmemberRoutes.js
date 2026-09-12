8b0aed1054face6c3573bddf90e5c5c7a5eddfbc
Fahd Khodair <khodairfahd@gmail.com>
2026-09-12 09:02:43 -0700

    fix project member routes

--- src/routes/projectmemberRoutes.js
+++ src/routes/projectmemberRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { addMember, removeMember } = require('../controllers/projectmembercontroller.js');
-const { projectPermissionMiddleware } = require('../middleware/projectPermissionMiddleware.js');
-const {validate} = require('../middleware/validateMiddleware.js');
+const projectPermissionMiddleware = require('../middleware/projectPermissionMiddleware.js');
+const validate = require('../middleware/validateMiddleware.js');
 const router = express.Router();
 router.post('/id/members',validate,projectPermissionMiddleware, addMember);
 router.delete('/:id/members/:userId',validate, projectPermissionMiddleware, removeMember);
